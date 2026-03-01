d1833bacb7978ac4bdac53a0fd9db0190c5ed5f7
Achintha Bandara <achinthabandara2023@gmail.com>
2026-03-01 16:32:56 +0530

    fixed some code errors in authcontroller.js

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -11,14 +11,14 @@ export async function registerUser(req, res) {
 
         // Create the user (Password hashing is handled in the User Model)
         const user = await User.create({ name, email, password });
-        res.status(201).json({
+        return res.status(201).json({
             message: 'Registration Successful.',
             user: { id: user._id, name: user.name, email: user.email }
         });
         
     } catch (error) {
         console.log("Error occurred:", error);
-        res.status(500).json({message: 'Server Error!'});
+        return res.status(500).json({message: 'Server Error!'});
     }
 }
 
@@ -36,13 +36,13 @@ export async function loginUser(rea, res) {
         // 3. Create and send JWT Token
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {expiresIn: '1d'});
 
-        res.status(200).json({
+        return res.status(200).json({
             token,
             user: { id: user._id, name: user.name, email: user.email }
         });
         
     } catch (error) {
         console.log("Error occurred:", error);
-        res.status(500).json({message: 'Server Error!'});
+        return res.status(500).json({message: 'Server Error!'});
     }
 }
