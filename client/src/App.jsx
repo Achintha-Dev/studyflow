c7e16ca5affbc96831948f98426fb59585516dd0
Achintha Bandara <achinthabandara2023@gmail.com>
2026-03-01 09:59:16 +0530

    installed all packages and dependencies in client and server folders

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <p className="text-xl">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque nobis deleniti distinctio, molestiae est veritatis nulla eveniet dolorum aliquam esse perferendis rem minima tenetur sint, quas ex omnis exercitationem dolor.</p>
+    </>
+  )
+}
+
+export default App
