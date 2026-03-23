a76a5fd7574caa651048a369dc0ea3cec8afa430
Achintha Bandara <achinthabandara2023@gmail.com>
2026-03-23 12:40:44 +0530

    Update cors origin in server.js

--- server/src/server.js
+++ server/src/server.js
@@ -14,7 +14,7 @@ import router from './routes/tasksRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({origin: 'http://localhost:5173'})); // use connect to frontend
+app.use(cors({origin: '*'})); // use connect to frontend
 app.use(express.json()); // allow to read json data.
 
 app.use(rateLimiter);
