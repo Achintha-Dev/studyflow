58bff3546a29c42f661c46da8aeb7e2bb2ca264c
Achintha Bandara <achinthabandara2023@gmail.com>
2026-03-23 14:00:45 +0530

    fix: update CORS origin for vercel deployment

--- server/src/server.js
+++ server/src/server.js
@@ -15,7 +15,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://studyflow-m6s23xs61-achintha-devs-projects.vercel.app"],
+    origin: [
+        "http://localhost:5173", 
+        "https://studyflow-eight-flame.vercel.app"
+    ],
     credentials: true
 })); // use connect to frontend
 app.use(express.json()); // allow to read json data.
